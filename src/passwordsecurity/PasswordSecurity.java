76ca6815fc1a6311bee96760ca6a13fec4956f1a
98kev <98kev@DESKTOP-0JBI4S9>
2024-08-15 21:35:28 -0700

    Iniciar

---
+++ src/passwordsecurity/PasswordSecurity.java
@@ -0,0 +1,16 @@
+
+package passwordsecurity;
+
+import javax.swing.JOptionPane;
+
+public class PasswordSecurity {
+
+    public static void main(String[] args) {
+        MenuInicio mostrar = new MenuInicio();
+        JOptionPane.showMessageDialog(null," Crea tu contraseña, es recomendable tener una contraseña de 12 caracteres ");
+        mostrar.setVisible(true);
+        
+
+    }
+    
+}
